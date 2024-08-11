2ecf1e7636691a6ec120e94735862246338fcff2
nkraj7266 <nkraj7266@gmail.com>
2024-08-11 06:15:00 +0530

    [add] designed homepage

--- src/components/custom/Footer.jsx
+++ src/components/custom/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
 	return (
-		<div className="flex flex-row justify-center items-center h-10 bg-gray-800 text-white">
+		<div className="flex flex-row justify-center items-center h-10 dark:bg-gray-800 dark:text-white bg-white border-t-2">
 			<p className="text-center">
-				Developed by Team 3 - Muskeeters ❤️ | © 2024 All rights reserved
+				Developed by Team [3 Muskeeters] ❤️ | © 2024 All rights reserved
 			</p>
 		</div>
 	);
